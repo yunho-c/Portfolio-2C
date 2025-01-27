998f7debacb2e15a3ac6a21a0fbbbeebacc2ee2f
Yunho Cho <yhjc2692@gmail.com>
2025-01-27 10:36:11 -0500

    Edited bio: less noise, more professional

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -11,9 +11,10 @@ export const site: SiteConfig = {
   author: {
     avatar: '/assets/maskable@512.png',
     name: 'Yunho Cho',
-    status: '👍',
+    status: '🎓',
     // bio: "I'm an engineer who's quite passionate in just about everything in the world. This includes mechanical engineering, software engineering, machine learning, human-computer interaction, design theory, cognitive psychology, 3D graphics, origami, musical instruments, spatial audio, and photography. In free time, I watch DW Documentary, lie down in bed thinking about things, write, plan out the coming weeks, and majestically procrastinate while doom-scrolling through Instagram. Thanks for visiting - and always feel free to reach out! Let's grab coffee and talk."
-    bio: "I'm an engineer who's quite passionate about many many things in the world, like: machines, software, AI, human-computer interaction, psychology, design theory, interactive artworks, 3D graphics, origami, music & instruments, spatial audio, and photography. \nIn my free time, I watch documentaries, think about things, ride the bike, climb rocks, read, write, plan out the coming weeks, and majestically procrastinate while doomscrolling through Instagram or YouTube. \nThanks for visiting – and feel free to reach out anytime! Let's grab coffee and talk."
+    // bio: "I am an engineer who is passionate about things like machines, software, machine learning, human-computer interaction, psychology, design theory, interactive artworks, 3D graphics, origami, music & instruments, spatial audio, and photography. \nIn my free time, I watch documentaries, think, ride a bike, read, and majestically procrastinate while doomscrolling through Instagram. \nThanks for visiting—and feel free to reach out anytime for a coffee chat!"
+    bio: "I'm an engineer passionate about assistive technology and human-computer interaction. \nIn my free time, I watch documentaries, think, ride a bike, read, and majestically procrastinate while doomscrolling through Instagram. \nThanks for visiting—and feel free to reach out anytime for a (virtual) coffee chat!"
   },
   themeColor: '#3D4451'
 }
